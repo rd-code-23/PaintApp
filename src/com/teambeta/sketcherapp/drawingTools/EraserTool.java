15761bff68e25293aafb2bd8a74218669a86486f
Kyle Saburao <ksaburao@sfu.ca>
2017-10-03 13:18:31 -0700

    Fix: #28 Tools - Brush - Improve operating behaviour
    1. Reimplement the eraser tool behaviour to follow the brush tool.
    It will no longer just generate boxes, but rather a smooth line.

--- src/com/teambeta/sketcherapp/drawingTools/EraserTool.java
+++ src/com/teambeta/sketcherapp/drawingTools/EraserTool.java
@@ -9,39 +9,51 @@ import java.awt.image.BufferedImage;
 
 /**
  * NOTE:
- * 1. sizeInPixels does nothing so far.
- * 2. The DrawArea class object must be passed into the tool to receive the background colour.
- * <p>
+ * The DrawArea class object must be passed into the tool to receive the background colour.
+ *
  * The EraserTool class implements the drawing behavior for when the Eraser tool has been selected
  */
 public class EraserTool extends DrawingTool {
 
-    private int currentY;
     private int currentX;
-    private int sizeInPixels;
+    private int currentY;
+    private int lastX;
+    private int lastY;
+    private Graphics2D layer1Graphics;
     private Color color;
+    private int brushWidth;
 
     /**
      * The constructor sets the properties of the tool to their default values
      */
     public EraserTool(DrawArea drawArea) {
         color = drawArea.getBackground();
-        sizeInPixels = 20;
+        brushWidth = 15;
         currentX = 0;
         currentY = 0;
+        lastX = 0;
+        lastX = 0;
+    }
+
+    public int getBrushWidth() {
+        return brushWidth;
+    }
+
+    public void setBrushWidth(int brushWidth) {
+        this.brushWidth = brushWidth;
     }
 
     @Override
     public void onDrag(BufferedImage canvas, BufferedImage[] layers, MouseEvent e) {
         //draw a path that follows your mouse while the mouse is being dragged
-        Graphics2D layer1Graphics = (Graphics2D) layers[0].getGraphics();
-        layer1Graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        layer1Graphics.setColor(color);
         currentX = e.getX();
         currentY = e.getY();
-        // draw line if graphics context not null
-        layer1Graphics.fillRect(currentX, currentY, sizeInPixels, sizeInPixels);
+
+        layer1Graphics.drawLine(lastX, lastY, currentX, currentY);
         DrawArea.drawLayersOntoCanvas(layers, canvas);
+
+        lastX = currentX;
+        lastY = currentY;
     }
 
     @Override
@@ -50,21 +62,21 @@ public class EraserTool extends DrawingTool {
 
     @Override
     public void onClick(BufferedImage canvas, BufferedImage[] layers, MouseEvent e) {
-        Graphics2D layer1Graphics = (Graphics2D) layers[0].getGraphics();
-        layer1Graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        layer1Graphics.setColor(color);
-
+        currentX = e.getX();
         currentY = e.getY();
-        //draw a point where the mouse was clicked
-        layer1Graphics.fillRect(currentX, currentY, sizeInPixels, sizeInPixels);
+
+        layer1Graphics.fillOval(currentX - (brushWidth/2),currentY - (brushWidth/2), brushWidth, brushWidth);
         DrawArea.drawLayersOntoCanvas(layers, canvas);
     }
 
     @Override
     public void onPress(BufferedImage canvas, BufferedImage[] layers, MouseEvent e) {
+        initLayer1Graphics(canvas, layers, e);
         //set the coordinates to the current point when the mouse is pressed
         currentX = e.getX();
         currentY = e.getY();
+        lastX = currentX;
+        lastY = currentY;
     }
 
     /**
@@ -76,4 +88,20 @@ public class EraserTool extends DrawingTool {
     public Color getColor() {
         return color;
     }
+
+    /**
+     * Initialize the parameters required for layer1Graphics.
+     *
+     * @param canvas for drawing the line onto.
+     * @param layers first layer by default is layers[0]
+     * @param e      MouseEvent
+     */
+    private void initLayer1Graphics(BufferedImage canvas, BufferedImage[] layers, MouseEvent e) {
+        layer1Graphics = (Graphics2D) layers[0].getGraphics();
+        layer1Graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        layer1Graphics.setColor(color);
+        layer1Graphics.setStroke(new BasicStroke(getBrushWidth(), BasicStroke.CAP_ROUND,    // End-cap style
+                BasicStroke.CAP_BUTT));
+    }
+
 }
