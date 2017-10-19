f1fd59f0d77f92a13a1061a9831ac443e20525bb
Darryl Fonseka <dfonseka@sfu.ca>
2017-10-18 21:35:42 -0700

    Implement escape option for text fields using the escape key

--- src/com/teambeta/sketcherapp/drawingTools/TextFieldInput.java
+++ src/com/teambeta/sketcherapp/drawingTools/TextFieldInput.java
@@ -1,8 +1,7 @@
 package com.teambeta.sketcherapp.drawingTools;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 import javax.swing.*;
 
@@ -25,23 +24,43 @@ public class TextFieldInput extends JDialog {
      */
     public TextFieldInput(Color textColor, int locationX, int locationY, Font font) {
         super(new JFrame(), true);
+        userInput = "";
         this.font = font;
         setUndecorated(true);
         setLocation(locationX - MOUSE_DEVIATION_X, locationY - MOUSE_DEVIATION_Y);
         setLayout(new BorderLayout());
         JTextField textField = new JTextField();
         textField.setFont(font);
+        textField.setFocusable(true);
         textField.setForeground(textColor);
         textField.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 userInput = textField.getText();
-                setVisible(false);
-                dispose();
+                closeTextField(textField);
             }});
-        setPreferredSize(new Dimension(WIDTH*font.getSize(), (HEIGHT*font.getSize())));
+        textField.addKeyListener(new KeyAdapter() {
+             @Override
+             public void keyPressed(KeyEvent e) {
+                 if (e.getKeyChar() == KeyEvent.VK_ESCAPE) {
+                     closeTextField(textField);
+                 }
+             }
+         }
+        );
+        setPreferredSize(new Dimension(WIDTH * font.getSize(), (HEIGHT * font.getSize())));
         getContentPane().add(BorderLayout.CENTER, textField);
         pack();
         setVisible(true);
+    }
+
+    /**
+     * Close text field.
+     *
+     * @param textField to close.
+     */
+    private void closeTextField(JTextField textField) {
+        setVisible(false);
+        dispose();
     }
 
     /**
