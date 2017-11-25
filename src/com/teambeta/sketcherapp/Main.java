6804c658178abbad2d2bd4b91d1aef75506ec95b
Darryl Fonseka <dfonseka@sfu.ca>
2017-11-24 18:05:51 -0800

    Fix #107: Implement splash screen

--- src/com/teambeta/sketcherapp/Main.java
+++ src/com/teambeta/sketcherapp/Main.java
@@ -1,5 +1,6 @@
 package com.teambeta.sketcherapp;
 
+import com.teambeta.sketcherapp.ui.BetaSplashScreen;
 import com.teambeta.sketcherapp.ui.MainUI;
 
 /**
@@ -8,12 +9,17 @@ import com.teambeta.sketcherapp.ui.MainUI;
 public class Main {
     private static final int DEFAULT_CANVAS_WIDTH = 1600;
     private static final int DEFAULT_CANVAS_HEIGHT = 900;
+    private static final int SPLASH_DISPLAY_DURATION = 5000;
+
     /**
      * Main program.
      *
      * @param args of user input
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        new BetaSplashScreen(SPLASH_DISPLAY_DURATION);
+
+        Thread.sleep(SPLASH_DISPLAY_DURATION);
         System.setProperty("sun.java2d.opengl", "True");
         MainUI mainUI = new MainUI(DEFAULT_CANVAS_WIDTH, DEFAULT_CANVAS_HEIGHT);
         mainUI.displayUI();
