c9e07b745f36c3e06b6d036345276e24f8970866
Kyle Saburao <ksaburao@sfu.ca>
2017-11-26 09:20:53 -0800

    Reduce individual dot size for airbrush

--- src/com/teambeta/sketcherapp/drawingTools/AirBrushTool.java
+++ src/com/teambeta/sketcherapp/drawingTools/AirBrushTool.java
@@ -22,7 +22,7 @@ public class AirBrushTool extends DrawingTool {
     private int dotDiameter;
     private int dotX;
     private int dotY;
-    private static final double DOT_WIDTH_RATIO = 0.50;
+    private static final double DOT_WIDTH_RATIO = 1.0 / 3.0;
     private static final int DEFAULT_DOT_DIAMETER = 20;
     private static final int DEFAULT_DOTS_TO_DRAW = 5;
     private static final int DEFAULT_STOKE_VALUE = 10;
